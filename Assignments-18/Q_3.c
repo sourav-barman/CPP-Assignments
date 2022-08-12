c8993bf47707dc8379481d0d30e939e43b417c1f
sourav-barman <78752547+sourav-barman@users.noreply.github.com>
2022-08-12 16:17:06 +0530

    Update Q_3.c

--- Assignments-18/Q_3.c
+++ Assignments-18/Q_3.c
@@ -30,9 +30,7 @@ int compareString(char str1[], char str2[]) {
     int i;
 
     for(i=0; str1[i] && str2[i]; i++) {
-        if (str1[i] == str2[i])
-            continue;
-        else 
+        if (str1[i] != str2[i])
             return (str1[i] - str2[i]);
     }
 
