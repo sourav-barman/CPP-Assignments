9ff9afda095fe185f63ebebdeea9e08dfc36b725
sourav-barman <78752547+sourav-barman@users.noreply.github.com>
2022-08-14 13:31:54 +0530

    Update Q_2.c

--- Assignments-16/Q_2.c
+++ Assignments-16/Q_2.c
@@ -1,29 +1,32 @@
 /*Write a program to calculate the product of two matrices each of order 3x3.*/
 
 #include <stdio.h>
-#define SIZE 3
 
 int main() {
-    int i, j, mat1[SIZE][SIZE], mat2[SIZE][SIZE];
+    int i, j, k, mat1[3][3], mat2[3][3], mat3[3][3];
 
     printf("Enter the elements of first matrix(3 x 3) row wise: \n");
-    for(i=0; i<SIZE; i++) {
-        for(j=0; j<SIZE; j++) {
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
             scanf("%d", &mat1[i][j]);
         }
     }
 
     printf("\nEnter the elements of second matrix(3 x 3) row wise: \n");
-    for(i=0; i<SIZE; i++) {
-        for(j=0; j<SIZE; j++) {
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
             scanf("%d", &mat2[i][j]);
         }
     }
 
     printf("\nProduct of Matrix1 and Matrix2:\n\t");
-    for(i=0; i<SIZE; i++) {
-        for(j=0; j<SIZE; j++) {
-            printf("%d ", mat1[i][j] * mat2[i][j]);
+    for(i=0; i<3; i++) {
+        for(j=0; j<3; j++) {
+            mat3[i][j] = 0;
+            for(k=0; k<3; k++) {
+                mat3[i][j] += (mat1[i][k] * mat2[k][j]);
+            }
+            printf("%d ", mat3[i][j]);
         }
         printf("\n\t");
     }
